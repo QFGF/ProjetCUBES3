388c72d96779e1373a6109196bbeb4574f1aa475
Loic Martin <loic.martin1@viacesi.fr>
2023-05-23 09:33:23 +0200

    Front

---
+++ HTML/loader.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+  <title>Page de chargement</title>
+  <link rel="stylesheet" type="text/css" href="../css/styles-loader.css">
+</head>
+<body>
+  <div class="loader"></div>
+</body>
+</html>
