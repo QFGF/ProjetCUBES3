b9bf70e06d89250fb9a240f21fd22ea5114fe73b
Fanny ALFONSO <fanny.alfonso@viacesi.fr>
2023-05-24 13:21:01 +0200

    Portion Google analytics => à ajouter au code

---
+++ BLOC 3/Commit/googleanalytics.php
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PQSEHM6B03"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PQSEHM6B03');
+</script>
