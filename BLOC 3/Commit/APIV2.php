d1754842111a211d89ca5420c05eee8e41aed9c6
QuentinFraise <fraisequentingit@gmail.com>
2023-06-14 16:56:27 +0200

    APIV2 structurer + CSS en cours

--- BLOC 3/Commit/APIV2.php
+++ BLOC 3/Commit/APIV2.php
@@ -1,5 +1,5 @@
 <?php
-echo '<link rel="stylesheet" type="text/css" href="../CSS/themes.css">';
+echo '<link rel="stylesheet" type="text/css" href="./APIV22">';
 // Requête de recherche
 $searchQuery = 'Mario';
 
@@ -7,8 +7,7 @@ $searchQuery = 'Mario';
 $apiKey = 'AIzaSyCNDwHu0VuRcXerDO5NssytkNymtQSHrfk';
 
 // Nombre de vidéos à récupérer
-$maxResults = 8;
-
+$maxResults = 8; // multiple de 4 !!!!
 // URL de l'API pour récupérer la liste de vidéos
 $url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' . urlencode($searchQuery) . '&maxResults=' . $maxResults . '&key=' . $apiKey;
 
@@ -18,9 +17,19 @@ $response = file_get_contents($url);
 // Convertir la réponse JSON en tableau associatif
 $data = json_decode($response, true);
 
-// Parcourir la liste de vidéos
+//compteur de lignes
+$compteur = 0;
 echo '<div class="container">';
+echo '<div class="ligne">';
+// Parcourir la liste de vidéos
 foreach ($data['items'] as $item) {
+    if ($compteur >= 4) { //Nombre de vidéo sur une ligne
+        $compteur = 0;
+        echo '</div>';
+        echo '<div class="ligne">';
+    } else {
+        $compteur = $compteur + 1;
+    }
     // Récupérer les détails de chaque vidéo
     $videoTitle = $item['snippet']['title'];
     $videoId = $item['id']['videoId'];
@@ -32,20 +41,22 @@ foreach ($data['items'] as $item) {
     $datad = json_decode($responsedetails, true) ; //****details***//
     $videoDuree = $datad['items'][0]['contentDetails']['duration'];
     // Vérifier si la requête a réussi
-    echo '<div>';
+    echo '<div class=contained>';
     if ($data['pageInfo']['totalResults'] > 0) {
+        echo '<div class=info>';
         // Afficher le titre et le lien de la vidéo
-        echo '<p>' . 'Titre : ' . $videoTitle . '</p>' . '<br>';
+        echo '<p class=textes><strong>' . 'Titre : </strong><br>' . $videoTitle . '</p>';
         //echo 'Lien: <a href="https://www.youtube.com/watch?v=' . $videoId . '">Regarder la vidéo</a><br>';
-        echo '<p>' . 'Description : ' . $videoDescription . '</p>' . '<br>' ; 
-        echo '<p>' . 'Durée : ' . $videoDuree . '</p>' . '<br>' ; 
+        echo '<p class=textes><strong>' . 'Description : </strong><br>' . $videoDescription . '</p>'; 
+        echo '<p class=textes><strong>' . 'Durée : </strong><br>' . $videoDuree . '</p>'; 
+        echo '</div>';
         //var_dump($responsedetails) ; // n'affiche pas 'contentDetails'
-        echo '<p>' . $video_code_integration . '</p>';
-        echo '<br>';
+        echo '<p class=video>' . $video_code_integration . '</p>';
     } else {
     echo '<p>' . 'Aucune vidéo trouvée pour la requête de recherche.' . '</p>';
     }
     echo '</div>';
 }
+echo '</div>'; 
 echo '</div>'
 ?>
