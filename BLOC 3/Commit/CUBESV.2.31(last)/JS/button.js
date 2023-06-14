97beeadabd1f32ed5e2004f816ad303058eda2a0
Loic Martin <loic.martin1@viacesi.fr>
2023-06-15 00:40:20 +0200

    Changement des ma co test admin par utlisateurs bdd

--- BLOC 3/Commit/CUBESV.2.31(last)/JS/button.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.querySelector(".popup-button");
-    const popup = document.querySelector(".popup");
-  
-    button.addEventListener("click", function() {
-      popup.classList.toggle("open");
-    });
-  });
